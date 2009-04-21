e8e81a2d2df9fa6cc00f3a6910c15b9e8c906fe1
mbostock <mbostock@99c00a00-a864-4cc4-b658-633078b1b672>
2009-04-21 06:02:36 +0000

    Another fix to the hacky function expression support.
    git-svn-id: svn+ssh://blit.stanford.edu/usr/graphics/svn/infovis/protovis@260 99c00a00-a864-4cc4-b658-633078b1b672

--- pv.js
+++ pv.js
@@ -5,7 +5,7 @@ try {
   eval("pv.parse = function(x) x;"); // native support
 } catch (e) {
   pv.parse = function(js) { // hacky regex support
-    var re = new RegExp("function\\([^)]*\\)\\s*", "mg"), m, i = 0;
+    var re = new RegExp("function(\\s+\\w+)?\\([^)]*\\)\\s*", "mg"), m, i = 0;
     var s = "";
     while (m = re.exec(js)) {
       var j = m.index + m[0].length;
