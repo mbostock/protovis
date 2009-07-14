6f2eeb516bbd103bc008d9e8cd9ea886f18fe991
mbostock <mbostock@99c00a00-a864-4cc4-b658-633078b1b672>
2009-07-14 18:37:43 +0000

    Color documentation.
    git-svn-id: svn+ssh://blit.stanford.edu/usr/graphics/svn/infovis/protovis@459 99c00a00-a864-4cc4-b658-633078b1b672

--- color/Color.js
+++ color/Color.js
@@ -1,18 +1,39 @@
-pv.Color = function(color, opacity) {
-  this.color = color;
-  this.opacity = opacity;
-};
-
-pv.color = function(style) {
-  if (!style || (style == "transparent")) {
+/**
+ * Returns the {@link Color} for the specified color format string. Colors may
+ * have an associated opacity, or alpha channel. Color formats are specified by
+ * CSS Color Modular Level 3, using either in RGB or HSL color space. For
+ * example:<ul>
+ *
+ * <li>#f00 // #rgb
+ * <li>#ff0000 // #rrggbb
+ * <li>rgb(255, 0, 0)
+ * <li>rgb(100%, 0%, 0%)
+ * <li>hsl(0, 100%, 50%)
+ * <li>rgba(0, 0, 255, 0.5)
+ * <li>hsla(120, 100%, 50%, 1)
+ *
+ * </ul>The SVG 1.0 color keywords names are also supported, such as "aliceblue"
+ * and yellowgreen". The "transparent" keyword is also supported for a fully-
+ * transparent color.
+ *
+ * <p>If the {@code format} argument is already an instance of {@code Color},
+ * the argument is returned with no further processing.
+ *
+ * @param format the color specification string, e.g., "#f00".
+ * @return the corresponding {@code Color}.
+ * @see http://www.w3.org/TR/SVG/types.html#ColorKeywords
+ * @see http://www.w3.org/TR/css3-color/
+ */
+pv.color = function(format) {
+  if (!format || (format == "transparent")) {
     return new pv.Color.Rgb(0, 0, 0, 0);
   }
-  if (style instanceof pv.Color) {
-    return style;
+  if (format instanceof pv.Color) {
+    return format;
   }
 
   /* Handle hsl, rgb. */
-  var m1 = /([a-z]+)\((.*)\)/i.exec(style);
+  var m1 = /([a-z]+)\((.*)\)/i.exec(format);
   if (m1) {
     var m2 = m1[2].split(","), a = 1;
     switch (m1[1]) {
@@ -43,9 +64,32 @@ pv.color = function(style) {
   }
 
   /* Otherwise, assume named colors. TODO allow lazy conversion to RGB. */
-  return new pv.Color(style, 1);
+  return new pv.Color(format, 1);
 };
 
+/**
+ * Represents an abstract (possibly translucent) color. The color is divided
+ * into two parts: the {@code color} attribute, an opaque color format string,
+ * and the {@code opacity} attribute, a float between 0 and 1. The color space
+ * is dependent on the implementing class; all colors should support the {@link
+ * #rgb} method to convert to RGB color space for interpolation.
+ *
+ * @param color an opaque color format string, such as "#f00".
+ * @param opacity the opacity, in [0,1].
+ */
+pv.Color = function(color, opacity) {
+  this.color = color;
+  this.opacity = opacity;
+};
+
+/**
+ * Represents a color in RGB space.
+ *
+ * @param r the red channel, an integer in [0,255].
+ * @param g the green channel, an integer in [0,255].
+ * @param b the blue channel, an integer in [0,255].
+ * @param a the alpha channel, a float in [0,1].
+ */
 pv.Color.Rgb = function(r, g, b, a) {
   pv.Color.call(this, a ? ("rgb(" + r + "," + g + "," + b + ")") : "none", a);
   this.r = r; // [0,255], integer
@@ -53,13 +97,17 @@ pv.Color.Rgb = function(r, g, b, a) {
   this.b = b; // [0,255], integer
   this.a = a; // [0,1]
 };
-
 pv.Color.Rgb.prototype = pv.extend(pv.Color);
+pv.Color.Rgb.prototype.rgb = function() { return this; };
 
-pv.Color.Rgb.prototype.rgb = function() {
-  return this;
-};
-
+/**
+ * Represents a color in HSL space.
+ *
+ * @param h the hue, an integer in [0,360].
+ * @param s the saturation, a float in [0,1].
+ * @param l the lightness, a float in [0,1].
+ * @param a the opacity, a float in [0,1].
+ */
 pv.Color.Hsl = function(h, s, l, a) {
   pv.Color.call(this, "hsl(" + h + "," + (s * 100) + "%," + (l * 100) + "%)", a);
   this.h = h; // [0,360]
@@ -67,20 +115,18 @@ pv.Color.Hsl = function(h, s, l, a) {
   this.l = l; // [0,1]
   this.a = a; // [0,1]
 };
-
 pv.Color.Hsl.prototype = pv.extend(pv.Color);
 
 /**
- * @return rgb in [0, 1]
- * @param h hue in degrees in [0, 360) (modulo)
- * @param s saturation in [0, 1] (clamp)
- * @param l luminosity in [0, 1] (clamp)
+ * Returns the RGB color equivalent to this HSL color.
+ *
+ * @see Color.Rgb
  */
 pv.Color.Hsl.prototype.rgb = function() {
   var h = this.h, s = this.s, l = this.l;
 
-  h = h % 360;
-  if (h < 0) h += 360;
+  /* Some simple corrections for h, s and l. */
+  h = h % 360; if (h < 0) h += 360;
   s = Math.max(0, Math.min(s, 1));
   l = Math.max(0, Math.min(l, 1));
 
