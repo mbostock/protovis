87719ad7af4b8d36284ce5cb6ef23599116184e5
mbostock <mbostock@99c00a00-a864-4cc4-b658-633078b1b672>
2009-07-14 19:24:39 +0000

    Label documentation.
    git-svn-id: svn+ssh://blit.stanford.edu/usr/graphics/svn/infovis/protovis@460 99c00a00-a864-4cc4-b658-633078b1b672

--- mark/Label.js
+++ mark/Label.js
@@ -1,7 +1,19 @@
 /**
- * Represents a text label.
+ * Represents a text label, allowing textual annotation of other marks or
+ * arbitrary text within the visualization. The character data must be plain
+ * text (unicode), though the text can be styled using the {@link #font}
+ * property. If rich text is needed, external HTML elements can be overlaid on
+ * the canvas by hand.
  *
- * <p>No support for events.
+ * <p>Labels are positioned using the box model, similarly to {@link Dot}. Thus,
+ * a label has no width or height, but merely a text anchor location. The text
+ * is positioned relative to this anchor location based on the {@link
+ * #textAlign}, {@link #textBaseline} and {@link #textMargin} properties.
+ * Furthermore, the text may be rotated using {@link #textAngle}.
+ *
+ * <p>Labels ignore events, so as to not interfere with event handlers on
+ * underlying marks, such as bars. In the future, we may support event handlers
+ * on labels.
  */
 pv.Label = function() {
   pv.Mark.call(this);
@@ -11,51 +23,67 @@ pv.Label.prototype.type = pv.Label;
 pv.Label.toString = function() { return "label"; };
 
 /**
- *
+ * The character data to render; a string. The default value of the text
+ * property is the identity function, meaning the label's associated datum will
+ * be rendered using its {@code toString}.
  */
 pv.Label.prototype.defineProperty("text");
 
 /**
- * The default font is 10px sans-serif.
+ * The font format, per the CSS Level 2 specification. The default font is "10px
+ * sans-serif", for consistency with the HTML 5 canvas element specification.
+ * Note that since text is not wrapped, any line-height property will be
+ * ignored. The other font-style, font-variant, font-weight, font-size and
+ * font-family properties are supported.
  *
  * @see http://www.w3.org/TR/CSS2/fonts.html#font-shorthand
  */
 pv.Label.prototype.defineProperty("font");
 
 /**
- *
+ * The rotation angle, in radians. Text is rotated clockwise relative to the
+ * anchor location. For example, with the default left alignment, an angle of
+ * Math.PI / 2 causes text to proceed downwards. The default angle is zero.
  */
 pv.Label.prototype.defineProperty("textAngle");
 
 /**
- * The text color.
+ * The text color. The name "textStyle" is used for consistency with "fillStyle"
+ * and "strokeStyle", although it might be better to rename this property (and
+ * perhaps use the same name as "strokeStyle"). The default color is black.
+ *
+ * @see Color
  */
 pv.Label.prototype.defineProperty("textStyle");
 
 /**
- * <ul>
+ * The horizontal text alignment. One of:<ul>
  *
  * <li>left
  * <li>center
  * <li>right
  *
- * </ul>
+ * </ul>The default horizontal alignment is left.
  */
 pv.Label.prototype.defineProperty("textAlign");
 
 /**
- * <ul>
+ * The vertical text alignment. One of:<ul>
  *
  * <li>top
  * <li>middle
  * <li>bottom
  *
- * </ul>
+ * </ul>The default vertical alignment is bottom.
  */
 pv.Label.prototype.defineProperty("textBaseline");
 
 /**
- *
+ * The text margin, in pixels. The margin should be used to pad (nudge) the text
+ * away from its anchor location, in a direction dependent on the horizontal and
+ * vertical alignment properties. For example, if the text is left- and
+ * middle-aligned, the margin shifts the text to the right. The default margin
+ * is 3 pixels.
  */
 pv.Label.prototype.defineProperty("textMargin");
 
@@ -70,7 +98,8 @@ pv.Label.prototype.defineProperty("textMargin");
 pv.Label.prototype.defineProperty("textShadow");
 
 /**
- *
+ * Default properties for labels. See the individual properties for the default
+ * values.
  */
 pv.Label.defaults = new pv.Label().extend(pv.Mark.defaults)
     .text(pv.identity)
@@ -82,7 +111,11 @@ pv.Label.defaults = new pv.Label().extend(pv.Mark.defaults)
     .textMargin(3);
 
 /**
+ * Updates the display for the specified label instance {@code s} in the scene
+ * graph. This implementation handles the text formatting for the label, as well
+ * as positional properties.
  *
+ * @param s a node in the scene graph; the instance of the dot to update.
  */
 pv.Label.prototype.updateInstance = function(s) {
   var v = s.svg;
