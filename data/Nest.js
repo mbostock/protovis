a11c582af26398dfae65300d690c04dbfa9e012e
mbostock <mbostock@99c00a00-a864-4cc4-b658-633078b1b672>
2009-07-15 23:39:04 +0000

    Nest documentation.
    git-svn-id: svn+ssh://blit.stanford.edu/usr/graphics/svn/infovis/protovis@478 99c00a00-a864-4cc4-b658-633078b1b672

--- data/Nest.js
+++ data/Nest.js
@@ -1,86 +1,163 @@
+/**
+ * Returns a {@link Nest} operator for the specified array. This is a
+ * convenience factory method, equivalent to {@code new pv.Nest(array)}.
+ *
+ * @see Nest
+ * @param array an array of elements to nest.
+ * @return a nest operator for the specified array.
+ */
 pv.nest = function(array) {
   return new pv.Nest(array);
 };
 
+/**
+ * Represents a {@link Nest} operator for the specified array. Nesting allows
+ * elements in an array to be grouped into a hierarchical tree structure. The
+ * levels in the tree are specified by <i>key</i> functions. The leaf nodes of
+ * the tree can be sorted by value, while the internal nodes can be sorted by
+ * key. Finally, the tree can be returned either has a multidimensional array
+ * via {@link #entries}, or as a hierarchical map via {@link #map}. The {@link
+ * #rollup} routine similarly returns a map, collapsing the elements in each
+ * leaf node using a summary function.
+ *
+ * <p>For example, consider the following tabular data structure of Barley
+ * yields, from various sites in Minnesota during 1931-2:
+ *
+ * <pre>{ yield: 27.00, variety: "Manchuria", year: 1931, site: "University Farm" },
+ * { yield: 48.87, variety: "Manchuria", year: 1931, site: "Waseca" },
+ * { yield: 27.43, variety: "Manchuria", year: 1931, site: "Morris" }, ...</pre>
+ *
+ * To facilitate visualization, it may be useful to nest the elements first by
+ * year, and then by variety, as follows:
+ *
+ * <pre>var nest = pv.nest(yields)
+ *     .key(function(d) d.year)
+ *     .key(function(d) d.variety)
+ *     .entries();</pre>
+ *
+ * This returns a nested array. Each element of the outer array is a key-values
+ * pair, listing the values for each distinct key:
+ *
+ * <pre>{ key: 1931, values: [
+ *   { key: "Manchuria", values: [
+ *       { yield: 27.00, variety: "Manchuria", year: 1931, site: "University Farm" },
+ *       { yield: 48.87, variety: "Manchuria", year: 1931, site: "Waseca" },
+ *       { yield: 27.43, variety: "Manchuria", year: 1931, site: "Morris" },
+ *       ...
+ *     ]},
+ *   { key: "Glabron", values: [
+ *       { yield: 43.07, variety: "Glabron", year: 1931, site: "University Farm" },
+ *       { yield: 55.20, variety: "Glabron", year: 1931, site: "Waseca" },
+ *       ...
+ *     ]},
+ *   ]},
+ * { key: 1932, values: ... }</pre>
+ *
+ * Further details, including sorting and rollup, is provided below on the
+ * corresponding methods.
+ *
+ * @param array an array of elements to nest.
+ * @return a nest operator for the specified array.
+ */
 pv.Nest = function(array) {
   this.array = array;
   this.keys = [];
 };
 
+/**
+ * Nests using the specified key function. Multiple keys may be added to the
+ * nest; the array elements will be nested in the order keys are specified.
+ *
+ * @param key a key function; must return a string or suitable map key.
+ * @return this.
+ */
 pv.Nest.prototype.key = function(key) {
   this.keys.push(key);
   return this;
 };
 
+/**
+ * Sorts the previously-added keys. The natural sort order is used by default
+ * (see {@link pv#naturalOrder}); if an alternative order is desired, {@code
+ * order} should be a comparator function. If this method is not called (i.e.,
+ * keys are <i>unsorted</i>), keys will appear in the order they appear in the
+ * underlying elements array. For example,
+ *
+ * <pre>pv.nest(yields)
+ *     .key(function(d) d.year)
+ *     .key(function(d) d.variety)
+ *     .sortKeys()
+ *     .entries()</pre>
+ *
+ * groups yield data by year, then variety, and sorts the variety groups
+ * lexicographically (since the variety attribute is a string).
+ *
+ * <p>Key sort order is only used in conjunction with {@link #entries}, which
+ * returns an array of key-values pairs. If the nest is used to construct a
+ * {@link #map} instead, keys are unsorted.
+ *
+ * @param order an optional comparator function.
+ * @return this.
+ */
 pv.Nest.prototype.sortKeys = function(order) {
   this.keys[this.keys.length - 1].order = order || pv.naturalOrder;
   return this;
 };
 
+/**
+ * Sorts the leaf values. The natural sort order is used by default (see {@link
+ * pv#naturalOrder}); if an alternative order is desired, {@code order} should
+ * be a comparator function. If this method is not called (i.e., values are
+ * <i>unsorted</i>), values will appear in the order they appear in the
+ * underlying elements array. For example,
+ *
+ * <pre>pv.nest(yields)
+ *     .key(function(d) d.year)
+ *     .key(function(d) d.variety)
+ *     .sortValues(function(a, b) a.yield - b.yield)
+ *     .entries()</pre>
+ *
+ * groups yield data by year, then variety, and sorts the values for each
+ * variety group by yield.
+ *
+ * <p>Value sort order, unlike keys, applies to both {@link #entries} and {@link
+ * #map}. It has no effect on {@link #rollup}.
+ *
+ * @param order an optional comparator function.
+ * @return this.
+ */
 pv.Nest.prototype.sortValues = function(order) {
   this.order = order || pv.naturalOrder;
   return this;
 };
 
-pv.Nest.prototype.rollup = function(f) {
-  var map = this.map();
-
-  function rollup(map) {
-    for (var key in map) {
-      var e = map[key];
-      if (e instanceof Array) {
-        map[key] = f(e);
-      } else {
-        rollup(e);
-      }
-    }
-  }
-
-  rollup(map);
-  return map;
-};
-
-pv.Nest.prototype.entries = function() {
-
-  function entries(map) {
-    var array = [];
-    for (var k in map) {
-      var v = map[k];
-      array.push({ key: k, values: (v instanceof Array) ? v : entries(v) });
-    };
-    return array;
-  }
-
-  function sort(array, i) {
-    var o = this.keys[i].order;
-    if (o) {
-      array.sort(function(a, b) { return o(a.key, b.key); });
-    }
-    if (++i < this.keys.length) {
-      for (var j = 0; j < array.length; j++) {
-        sort.call(this, array[j].values, i);
-      }
-    }
-    return array;
-  }
-
-  return sort.call(this, entries(this.map()), 0);
-};
-
+/**
+ * Returns a hierarchical map of values. Each key adds one level to the
+ * hierarchy. With only a single key, the returned map will have a key for each
+ * distinct value of the key function; the correspond value with be an array of
+ * elements with that key value. If a second key is added, this will be a nested
+ * map. For example:
+ *
+ * <pre>pv.nest(yields)
+ *     .key(function(d) d.variety)
+ *     .key(function(d) d.site)
+ *     .map()</pre>
+ *
+ * returns a map {@code m} such that {@code m[variety][site]} is an array, a subset of
+ * {@code yields}, with each element having the given variety and site.
+ *
+ * @return a hierarchical map of values.
+ */
 pv.Nest.prototype.map = function() {
-  if (!this.keys.length) {
-    return this.array;
-  }
-
   var map = {}, values = [];
+
+  /* Build the map. */
   for (var i, j = 0; j < this.array.length; j++) {
     var x = this.array[j];
     var m = map;
     for (i = 0; i < this.keys.length - 1; i++) {
       var k = this.keys[i](x);
-      if (!m[k]) {
-        m[k] = {};
-      }
+      if (!m[k]) m[k] = {};
       m = m[k];
     }
     k = this.keys[i](x);
@@ -92,6 +169,7 @@ pv.Nest.prototype.map = function() {
     m[k].push(x);
   }
 
+  /* Sort each leaf array. */
   if (this.order) {
     for (var i = 0; i < values.length; i++) {
       values[i].sort(this.order);
@@ -99,4 +177,78 @@ pv.Nest.prototype.map = function() {
   }
 
   return map;
+};
+
+/**
+ * Returns a hierarchical nested array. This method is similar to {@link
+ * pv#entries}, but works recursively on the entire hierarchy. Rather than
+ * returning a map like {@link #map}, this method returns a nested array. Each
+ * element of the array has a {@code key} and {@code values} field. For leaf
+ * nodes, the {@code values} array will be a subset of the underlying elements
+ * array; for non-leaf nodes, the {@code values} array will contain more
+ * key-values pairs.
+ *
+ * <p>For an example usage, see the {@link Nest} constructor.
+ *
+ * @return a hierarchical nested array.
+ */
+pv.Nest.prototype.entries = function() {
+
+  /** Recursively extracts the entries for the given map. */
+  function entries(map) {
+    var array = [];
+    for (var k in map) {
+      var v = map[k];
+      array.push({ key: k, values: (v instanceof Array) ? v : entries(v) });
+    };
+    return array;
+  }
+
+  /** Recursively sorts the values for the given key-values array. */
+  function sort(array, i) {
+    var o = this.keys[i].order;
+    if (o) array.sort(function(a, b) { return o(a.key, b.key); });
+    if (++i < this.keys.length) {
+      for (var j = 0; j < array.length; j++) {
+        sort.call(this, array[j].values, i);
+      }
+    }
+    return array;
+  }
+
+  return sort.call(this, entries(this.map()), 0);
+};
+
+/**
+ * Returns a rollup map. The behavior of this method is the same as {@link
+ * #map}, except that the leaf values are replaced with the return value of the
+ * specified rollup function {@code f}. For example,
+ *
+ * <pre>pv.nest(yields)
+ *      .key(function(d) d.site)
+ *      .rollup(function(v) pv.median(v, function(d) d.yield))</pre>
+ *
+ * first groups yield data by site, and then returns a map from site to median
+ * yield for the given site.
+ *
+ * @see #map
+ * @param f a rollup function.
+ * @return a hierarhical map, with the leaf values computed by {@code f}.
+ */
+pv.Nest.prototype.rollup = function(f) {
+
+  /** Recursively descends to the leaf nodes (arrays) and does rollup. */
+  function rollup(map) {
+    for (var key in map) {
+      var value = map[key];
+      if (value instanceof Array) {
+        map[key] = f(value);
+      } else {
+        rollup(value);
+      }
+    }
+    return map;
+  }
+
+  return rollup(this.map());
 };
