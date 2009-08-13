e2ed3cb15efde07d275d8f952b60887c4c41401a
mbostock <mbostock@99c00a00-a864-4cc4-b658-633078b1b672>
2009-08-13 20:16:24 +0000

    Checkpoint VML support: Image.
    git-svn-id: svn+ssh://blit.stanford.edu/usr/graphics/svn/infovis/protovis@561 99c00a00-a864-4cc4-b658-633078b1b672

---
+++ sprite/vml/VmlImage.js
@@ -0,0 +1,40 @@
+pv.VmlImage = function() {};
+pv.VmlImage.prototype = pv.extend(pv.VmlSprite);
+
+pv.VmlImage.prototype.update = function() {
+  var vml = this.$dom;
+
+  /* Create VML elements as needed. */
+  if (this.visible) {
+    if (!vml) {
+      vml = this.$dom = {root: this.insert("v:image")};
+      vml.root.appendChild(vml.fill = this.create("v:fill"));
+      vml.root.appendChild(vml.stroke = this.create("v:stroke"));
+      vml.root.filled = true;
+      vml.root.stroked = true;
+    }
+    vml.root.style.display = "";
+  } else {
+    if (vml) vml.root.style.display = "none";
+    return;
+  }
+
+  vml.root.style.left = this.left;
+  vml.root.style.top = this.top;
+  vml.root.style.width = this.width;
+  vml.root.style.height = this.height;
+  vml.root.style.cursor = this.cursor;
+  vml.root.src = this.url;
+  vml.root.title = this.title || "";
+
+  var fill = pv.color(this.fillStyle);
+  vml.fill.color = fill.color;
+  vml.fill.opacity = fill.opacity;
+  var stroke = pv.color(this.strokeStyle);
+  vml.stroke.color = stroke.color;
+  vml.stroke.opacity = stroke.opacity;
+  vml.stroke.weight = this.lineWidth + "px";
+
+  /* events */
+  this.listen(vml.root);
+};
