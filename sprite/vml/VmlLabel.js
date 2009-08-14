dcf675bb04eee1025752eb834d38199e5fbf14b9
mbostock <mbostock@99c00a00-a864-4cc4-b658-633078b1b672>
2009-08-14 06:42:57 +0000

    Improvements to VML text rendering: rotation, alignment, baseline.
    git-svn-id: svn+ssh://blit.stanford.edu/usr/graphics/svn/infovis/protovis@567 99c00a00-a864-4cc4-b658-633078b1b672

--- sprite/vml/VmlLabel.js
+++ sprite/vml/VmlLabel.js
@@ -1,4 +1,5 @@
-/* TODO alignment, baseline, rotation? */
+/* TODO font-size detection for baseline adjustment */
+/* TODO textMargin support */
 
 pv.VmlLabel = function() {};
 pv.VmlLabel.prototype = pv.extend(pv.VmlSprite);
@@ -10,7 +11,15 @@ pv.VmlLabel.prototype.update = function() {
   if (this.visible) {
     if (!vml) {
       vml = this.$dom = {root: this.insert("v:shape")};
-      vml.root.appendChild(vml.text = this.create("v:textbox"));
+      vml.root.appendChild(vml.path = this.create("v:path"));
+      vml.root.appendChild(vml.fill = this.create("v:fill"));
+      vml.root.appendChild(vml.text = this.create("v:textpath"));
+      vml.root.filled = true;
+      vml.root.stroked = false;
+      vml.root.style.width = "100%";
+      vml.root.style.height = "100%";
+      vml.path.textpathok = true;
+      vml.text.on = true;
     }
     vml.root.style.display = "";
   } else {
@@ -18,12 +27,48 @@ pv.VmlLabel.prototype.update = function() {
     return;
   }
 
-  vml.root.style.left = this.left;
-  vml.root.style.top = this.top;
+  var dx = 0, dy = 0, size = 10;
+  switch (this.textBaseline) {
+    case "top": {
+      dx = Math.round(-Math.sin(this.textAngle) * size);
+      dy = Math.round(Math.cos(this.textAngle) * size);
+      break;
+    }
+    case "bottom": {
+      dx = -Math.round(-Math.sin(this.textAngle) * size);
+      dy = -Math.round(Math.cos(this.textAngle) * size);
+      break;
+    }
+  }
 
-  var fill = pv.color(this.textStyle)
-  vml.text.style.color = fill.color;
-  vml.text.style.opacity = fill.opacity;
+  vml.root.style.left = this.left + dx;
+  vml.root.style.top = this.top + dy;
+
+  var fill = pv.color(this.textStyle);
+  vml.fill.color = fill.color;
+  vml.fill.opacity = fill.opacity;
+
+  var x = Math.round(Math.cos(this.textAngle) * 1000),
+      y = Math.round(Math.sin(this.textAngle) * 1000), p;
+  switch (this.textAlign) {
+    case "right": {
+      p = "M" + -x + "," + -y + "L0,0";
+      break;
+    }
+    case "center": {
+      p = "M" + -x + "," + -y + "L" + x + "," + y;
+      break;
+    }
+    default: {
+      p = "M0,0L" + x + "," + y;
+      break;
+    }
+  }
+  vml.path.v = p;
+
   vml.text.style.font = this.font;
-  vml.text.innerText = this.text;
+  vml.text.style.color = "black";
+  vml.text.style["alignment-baseline"] = "alphabetic";
+  vml.text.style["v-text-align"] = this.textAlign;
+  vml.text.string = this.text;
 };
