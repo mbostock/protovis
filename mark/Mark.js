bf63b8b09ee76434373e28e660479d46d0290972
jheer <jheer@99c00a00-a864-4cc4-b658-633078b1b672>
2009-09-03 18:32:18 +0000

    Fixed jsdoc typo
    git-svn-id: svn+ssh://blit.stanford.edu/usr/graphics/svn/infovis/protovis@625 99c00a00-a864-4cc4-b658-633078b1b672

--- mark/Mark.js
+++ mark/Mark.js
@@ -509,7 +509,7 @@ pv.Mark.prototype.sibling = function() {
 
 /**
  * Returns the current instance in the scene graph of this mark, in the previous
- * instance of the enclsoing parent panel. May return null if this instance
+ * instance of the enclosing parent panel. May return null if this instance
  * could not be found. See the {@link pv.Layout#stack} function for an example
  * property function using cousin.
  *
