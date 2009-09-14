baf5595f55debc9354ba06e94365301001327660
mbostock <mbostock@99c00a00-a864-4cc4-b658-633078b1b672>
2009-09-14 20:39:15 +0000

    Checkpoint new jsdocs (LinearScale, LogScale).
    git-svn-id: svn+ssh://blit.stanford.edu/usr/graphics/svn/infovis/protovis@709 99c00a00-a864-4cc4-b658-633078b1b672

--- data/LinearScale.js
+++ data/LinearScale.js
@@ -3,7 +3,8 @@
  * constructor are optional, and equivalent to calling {@link #domain}.
  *
  * @class Represents a linear scale. <style
- * type="text/css">sub{line-height:0}</style> Most commonly, a linear scale
+ * type="text/css">sub{line-height:0}</style> <img src="../../ex/linear.png"
+ * width="180" height="175" align="right"> Most commonly, a linear scale
  * represents a 1-dimensional linear transformation from a numeric domain of
  * input data [<i>d<sub>0</sub></i>, <i>d<sub>1</sub></i>] to a numeric range of
  * pixels [<i>r<sub>0</sub></i>, <i>r<sub>1</sub></i>]. The equation for such a
@@ -28,7 +29,7 @@
  * <pre>.height(pv.Scale.linear(0, 100).range(0, 640))</pre>
  *
  * As you can see, scales do not always make code smaller, but they should make
- * code more explicit and easier to modify. In addition to readability, scales
+ * code more explicit and easier to maintain. In addition to readability, scales
  * offer several useful features:
  *
  * <p>1. The range can be expressed in colors, rather than pixels. Changing the
@@ -81,10 +82,49 @@ pv.Scale.linear = function() {
   }
 
   /**
+   * Sets or gets the input domain. This method can be invoked several ways:
+   *
+   * <p>1. <tt>domain(min, ..., max)</tt>
+   *
+   * <p>Specifying the domain as a series of numbers is the most explicit and
+   * recommended approach. Most commonly, two numbers are specified: the minimum
+   * and maximum value. However, for a diverging scale, or other subdivided
+   * poly-linear scales, multiple values can be specified. Values can be derived
+   * from data using {@link pv.min} and {@link pv.max}. For example:
+   *
+   * <pre>.domain(0, pv.max(array))</pre>
+   *
+   * An alternative method for deriving minimum and maximum values from data
+   * follows.
+   *
+   * <p>2. <tt>domain(array, minf, maxf)</tt>
+   *
+   * <p>When both the minimum and maximum value are derived from data, the
+   * arguments to the <tt>domain</tt> method can be specified as the array of
+   * data, followed by zero, one or two accessor functions. For example, if the
+   * array of data is just an array of numbers:
+   *
+   * <pre>.domain(array)</pre>
+   *
+   * On the other hand, if the array elements are objects representing stock
+   * values per day, and the domain should consider the stock's daily low and
+   * daily high:
+   *
+   * <pre>.domain(array, function(d) d.low, function(d) d.high)</pre>
+   *
+   * The first method of setting the domain is preferred because it is more
+   * explicit; setting the domain using this second method should be used only
+   * if brevity is required.
+   *
+   * <p>3. <tt>domain()</tt>
+   *
+   * <p>Invoking the <tt>domain</tt> method with no arguments returns the
+   * current domain as an array of numbers.
+   *
    * @function
    * @name pv.Scale.linear.prototype.domain
    * @param {number...} domain... domain values.
-   * @returns {pv.Scale.linear} <tt>this</tt>.
+   * @returns {pv.Scale.linear} <tt>this</tt>, or the current domain.
    */
   scale.domain = function(array, min, max) {
     if (arguments.length) {
@@ -101,10 +141,31 @@ pv.Scale.linear = function() {
   };
 
   /**
+   * Sets or gets the output range. This method can be invoked several ways:
+   *
+   * <p>1. <tt>range(min, ..., max)</tt>
+   *
+   * <p>The range may be specified as a series of numbers or colors. Most
+   * commonly, two numbers are specified: the minimum and maximum pixel values.
+   * For a color scale, values may be specified as {@link pv.Color}s or
+   * equivalent strings. For a diverging scale, or other subdivided poly-linear
+   * scales, multiple values can be specified. For example:
+   *
+   * <pre>.range("red", "white", "green")</pre>
+   *
+   * <p>Currently, only numbers and colors are supported as range values. The
+   * number of range values must exactly match the number of domain values, or
+   * the behavior of the scale is undefined.
+   *
+   * <p>2. <tt>range()</tt>
+   *
+   * <p>Invoking the <tt>range</tt> method with no arguments returns the current
+   * range as an array of numbers or colors.
+   *
    * @function
    * @name pv.Scale.linear.prototype.range
    * @param {...} range... range values.
-   * @returns {pv.Scale.linear} <tt>this</tt>.
+   * @returns {pv.Scale.linear} <tt>this</tt>, or the current range.
    */
   scale.range = function(start, end) {
     if (arguments.length) {
@@ -119,10 +180,20 @@ pv.Scale.linear = function() {
   };
 
   /**
+   * Inverts the specified value in the output range, returning the
+   * corresponding value in the input domain. This is frequently used to convert
+   * the mouse location (see {@link pv.Mark#mouse}) to a value in the input
+   * domain. Inversion is only supported for numeric ranges, and not colors.
+   *
+   * <p>Note that this method does not do any rounding or bounds checking. If
+   * the input domain is discrete (e.g., an array index), the returned value
+   * should be rounded. If the specified <tt>y</tt> value is outside the range,
+   * the returned value may be equivalently outside the input domain.
+   *
    * @function
    * @name pv.Scale.linear.prototype.invert
-   * @param {number} y
-   * @returns {number}
+   * @param {number} y a value in the output range (a pixel location).
+   * @returns {number} a value in the input domain.
    */
   scale.invert = function(y) {
     var j = pv.search(r, y);
@@ -132,9 +203,14 @@ pv.Scale.linear = function() {
   };
 
   /**
+   * Returns an array of evenly-spaced, suitably-rounded values in the input
+   * domain. This method attempts to return between 5 and 10 tick values. These
+   * values are frequently used in conjunction with {@link pv.Rule} to display
+   * tick marks or grid lines.
+   *
    * @function
    * @name pv.Scale.linear.prototype.ticks
-   * @returns {number[]}
+   * @returns {number[]} an array input domain values to use as ticks.
    */
   scale.ticks = function() {
     var min = d[0],
@@ -149,6 +225,14 @@ pv.Scale.linear = function() {
   };
 
   /**
+   * "Nices" this scale, extending the bounds of the input domain to
+   * evenly-rounded values. Nicing is useful if the domain is computed
+   * dynamically from data, and may be irregular. For example, given a domain of
+   * [0.20147987687960267, 0.996679553296417], a call to <tt>nice()</tt> might
+   * extend the domain to [0.2, 1].
+   *
+   * <p>This method must be invoked each time after setting the domain.
+   *
    * @function
    * @name pv.Scale.linear.prototype.nice
    * @returns {pv.Scale.linear} <tt>this</tt>.
@@ -162,10 +246,29 @@ pv.Scale.linear = function() {
   };
 
   /**
+   * Returns a view of this scale by the specified accessor function <tt>f</tt>.
+   * Given a scale <tt>y</tt>, <tt>y.by(function(d) d.foo)</tt> is equivalent to
+   * <tt>function(d) y(d.foo)</tt>.
+   *
+   * <p>This method is provided for convenience, such that scales can be
+   * succinctly defined inline. For example, given an array of data elements
+   * that have a <tt>score</tt> attribute with the domain [0, 1], the height
+   * property could be specified as:
+   *
+   * <pre>.height(pv.Scale.linear().range(0, 480).by(function(d) d.score))</pre>
+   *
+   * This is equivalent to:
+   *
+   * <pre>.height(function(d) d.score * 480)</pre>
+   *
+   * This method should be used judiciously; it is typically more clear to
+   * invoke the scale directly, passing in the value to be scaled.
+   *
    * @function
    * @name pv.Scale.linear.prototype.by
-   * @param {function} f
-   * @returns {pv.Scale.linear} the new view.
+   * @param {function} f an accessor function.
+   * @returns {pv.Scale.linear} a view of this scale by the specified accessor
+   * function.
    */
   scale.by = function(f) {
     function by() { return scale(f.apply(this, arguments)); }
