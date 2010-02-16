484b23ccf849d08342c37ff48e14c643264d907e
Mike Bostock <mbostock+gitorious@gmail.com>
2010-02-15 16:46:31 -0800

    Add pv.Format.number.
    This class is modeled loosely on Java's DecimalFormat class, and allows easy
    formatting and parsing of numbers. The main supported features are padding /
    rounding / truncation of integer and fraction parts, and group (thousands)
    separators.

--- src/text/Format.js
+++ src/text/Format.js
@@ -1,1 +1,13 @@
 pv.Format = {};
+
+/**
+ * @private Given a string that may be used as part of a regular expression,
+ * this methods returns an appropriately quoted version of the specified string,
+ * with any special characters escaped.
+ *
+ * @param {string} s a string to quote.
+ * @returns {string} the quoted string.
+ */
+function re_quote(s) {
+  return s.replace(/[\\\^\$\*\+\?\[\]\(\)\.\{\}]/g, "\\$&");
+}
