d87e98afd4a22c595d347bba2326870779071280
Mike Bostock <mbostock+gitorious@gmail.com>
2010-03-06 11:21:41 -0800

    Force layout now uses properties.

--- src/layout/Force.js
+++ src/layout/Force.js
@@ -1,61 +1,92 @@
-pv.Layout.force = function(nodes, links) {
-  var layout = {
-          charge: pv.Force.charge(),
-          drag: pv.Force.drag(),
-          spring: pv.Force.spring()
-        },
-      sim;
+pv.Layout.Force = function() {
+  pv.Layout.call(this);
+  var that = this;
 
-  /** @private */
-  function data() {
-    if (!sim) {
-      /* Initialize position using a random walk from the center. */
-      var x = this.parent.width() / 2,
-          y = this.parent.height() / 2;
-      for (var i = 0, n, a; i < nodes.length; i++) {
-        n = nodes[i];
-        a = Math.random() * 2 * Math.PI;
-        n.x = x += 4 * Math.cos(a);
-        n.y = y += 4 * Math.sin(a);
-      }
+  /** @private Initialize position using a random walk from the center. */
+  function init() {
+    var nodes = that.nodes(), links = that.links();
+    if (nodes.sim) return;
 
-      sim = pv.simulation(nodes);
-      sim.force(layout.charge);
-      sim.force(layout.drag);
-      sim.force(layout.spring.links(links));
-      sim.step();
-    }
-    return nodes;
-  }
-
-  layout.nodes = nodes = nodes.map(function(d, i) {
-      return {nodeName: i, nodeValue: d, linkDegree: 0};
+    nodes.forEach(function(d, i) {
+      nodes[i] = {nodeName: i, nodeValue: d, linkDegree: 0};
     });
-
-  layout.links = links = links.map(function(d) {
+    links.forEach(function(d, i) {
       var s = nodes[d.source],
           t = nodes[d.target],
           v = isNaN(d.value) ? 1 : d.value;
       s.linkDegree += v;
       t.linkDegree += v;
-      return {sourceNode: s, targetNode: t, linkValue: v};
+      links[i] = {sourceNode: s, targetNode: t, linkValue: v};
     });
 
-  layout.node = new pv.Mark()
-      .data(data)
+    var x = this.parent.width() / 2,
+        y = this.parent.height() / 2;
+    for (var i = 0, n = nodes.length; i < n; i++) {
+      var node = nodes[i], angle = Math.random() * 2 * Math.PI;
+      node.x = x += 4 * Math.cos(angle);
+      node.y = y += 4 * Math.sin(angle);
+    }
+
+    nodes.sim = pv.simulation(nodes);
+    nodes.sim.force(pv.Force.drag());
+    nodes.sim.force(pv.Force.charge());
+    nodes.sim.force(pv.Force.spring().links(links));
+
+    var i = 0, n = that.iterations();
+    if (that.interactive()) {
+      var t = setInterval(function() {
+          if (i++ < n) {
+            nodes.sim.step();
+            that.root.render();
+          } else {
+            clearInterval(t);
+          }
+        }, 42);
+    } else while (i++ < n) {
+      nodes.sim.step();
+    }
+  }
+
+  this.node = new pv.Mark()
+      .def("init", init)
+      .data(function() { return that.nodes(); })
       .strokeStyle("#1f77b4")
       .fillStyle("#fff")
       .left(function(n) { return n.x; })
       .top(function(n) { return n.y; });
 
-  layout.link = new pv.Mark().extend(layout.node)
+  this.node.add = function(type) {
+      var mark = that.add(type).extend(this);
+      mark.link = that.link;
+      mark.node = that.node;
+      return mark;
+    };
+
+  this.link = new pv.Mark()
+      .extend(this.node)
       .data(function(p) { return [p.sourceNode, p.targetNode]; })
       .fillStyle(null)
       .lineWidth(function(d, p) { return Math.sqrt(p.linkValue) * 1.5; })
       .strokeStyle("rgba(0,0,0,.2)");
 
-  layout.step = function() { sim.step(); };
-
-  return layout;
+  this.link.add = function(type) {
+      var mark = that.add(pv.Panel)
+          .def("init", init)
+          .data(function() { return that.links(); })
+          .add(type).extend(this);
+      mark.link = that.link;
+      mark.node = that.node;
+      return mark;
+    };
 };
 
+pv.Layout.Force.prototype = pv.extend(pv.Layout)
+    .property("nodes")
+    .property("links")
+    .property("interactive", Boolean)
+    .property("iterations", Number);
+
+pv.Layout.Force.prototype.defaults = new pv.Layout.Force()
+    .extend(pv.Layout.prototype.defaults)
+    .interactive(true)
+    .iterations(200);
