cfc2d1a95ed189eded09470efc6d0cdd5151f346
Vadim Ogievetsky <vadim@ubuntu.(none)>
2010-03-09 12:17:01 -0800

    Added LayLon description

---
+++ src/data/LatLon.js
@@ -0,0 +1,25 @@
+/**
+ * A coordinate class used by the {@code pv.scale.GeoScale}. There is no
+ * explicit constructor corresponding to the class {@code pv.LatLon}; this
+ * class merely serves to document the attributes that are present in coordinate
+ * descriptions
+ *
+ * @class pv.LatLon
+ */
+
+/**
+ * The <i>latitude</i>-of the coordinate in degrees.
+ * Positive is North
+ *
+ * @type number
+ * @field pv.LatLon.prototype.lat
+ */
+
+/**
+ * The <i>longitude</i>-of the coordinate in degrees.
+ * Positive is East
+ *
+ * @type number
+ * @field pv.LatLon.prototype.lon
+ */
+
