ad52ab80a201fd91d3cc875d18daada9a733ff11
Mike Bostock <mbostock+gitorious@gmail.com>
2010-03-27 15:40:24 -0700

    Simplify stack layout using panels.
    In the same vein as commit b4f6884, stack layout now extends panel. Rather than
    performing a complex manipulation of the bind and build process, the stack
    layout uses new psuedo-properties `step` and `size` to compute the layout. The
    data is now defined using the `layers` property, which allows the stack layout
    to be computed as a side-effect of the inherited data property.
    
    I have temporarily removed support for labels with stack layouts. Although that
    would be possible, I'd like to have that supported through improvements in the
    anchor system.

--- src/layout/Stack.js
+++ src/layout/Stack.js
@@ -4,278 +4,262 @@
  * @class A layout for stacking marks vertically or horizontally. For example,
  *
  * <pre>vis.add(pv.Layout.Stack)
- *     .data([[1, 1.2, 1.7, 1.5, 1.7],
- *            [.5, 1, .8, 1.1, 1.3],
- *            [.2, .5, .8, .9, 1]])
- *   .add(pv.Area)
- *     .height(function(d) d * 40)
- *     .left(function() this.index * 35);</pre>
+ *     .layers([[1, 1.2, 1.7, 1.5, 1.7],
+ *              [.5, 1, .8, 1.1, 1.3],
+ *              [.2, .5, .8, .9, 1]])
+ *     .step(function() this.index * 35)
+ *     .size(function(d) d * 40)
+ *   .layer.add(pv.Area);</pre>
  *
  * specifies a vertically-stacked area chart.
  *
  * @returns {pv.Layout.Stack} a stack layout.
  */
 pv.Layout.Stack = function() {
-  pv.Layout.call(this);
-  var that = this;
+  pv.Panel.call(this);
+  var none = function() { return null; },
+      prop = {t: none, l: none, r: none, b: none, w: none, h: none};
 
-  /** @private */
-  var label = new pv.Mark()
-      .data(pv.identity)
-      .visible(function() {
-          return this.index == that.scene.$stack.max[this.parent.index];
-        })
-      .textBaseline("middle")
-      .textAlign("center");
-
-  /**
-   * Adds a mark of the specified type to a new panel, using this stack layout.
-   * Any positional properties defined on the returned mark will be evaluated
-   * immediately after the panel's data, allowing the layout to compute the
-   * implied offset.
-   */
-  this.add = function(type) {
-      var mark = that.parent.add(pv.Panel)
-              .extend(that)
-              .data(function() {
-                  var data = that.scene.$stack.data;
-                  that.prebuild(data, this.children[0]);
-                  return data;
-                })
-              .visible(function() { return that.scene[this.index].visible; })
-            .add(type)
-              .data(pv.identity),
-          bind = mark.bind;
-      mark.bind = function() { that.prebind(bind, this); };
-
-      /** @private Returns the target instance of the mark. */
-      function target() {
-        return mark.parent.scene[this.parent.index].children[0][this.index];
-      }
-
-      /** The label prototype; bound to the added mark. */
-      mark.label = new pv.Mark()
-          .extend(label)
-          .left(function() {
-              var s = target.call(this);
-              return s.left + s.width / 2;
-            })
-          .bottom(function() {
-              var s = target.call(this);
-              return s.bottom + s.height / 2;
-            });
-
-      /**
-       * Adds a label of the specified type (typically pv.Label) to a new panel
-       * using this stack layout. One label will be rendered per series, at the
-       * maximum value of the series.
-       */
-      mark.label.add = function(type) {
-          return that.parent.add(pv.Panel)
-              .data(function() { return that.scene.$stack.data; })
-            .add(type)
-              .extend(this);
-        };
-
-      return mark;
-    };
-};
-
-pv.Layout.Stack.prototype = pv.extend(pv.Layout)
-    .property("orient", String)
-    .property("order", String)
-    .property("offset", String);
-
-pv.Layout.Stack.prototype.defaults = new pv.Layout.Stack()
-    .extend(pv.Layout.prototype.defaults)
-    .orient("bottom")
-    .offset("zero");
-
-/** @private Capture the panel data on initialization. */
-pv.Layout.Stack.prototype.init = function(data) {
-  this.scene.$stack = {data: data};
-};
-
-/**
- * @private Before the child is bound, capture the original positional property
- * definitions so that the layout can use these properties to compute the
- * offset.
- */
-pv.Layout.Stack.prototype.prebind = function(bind, child) {
-  var o = child.binds ? child.binds.$stack : {};
-  bind.call(child);
-
-  /** @private The positional properties for dynamic substitution. */
-  var positionals = {
-    "left": 1,
-    "right": 1,
-    "top": 1,
-    "bottom": 1,
-    "width": 1,
-    "height": 1
-  };
-
-  /** @private Returns a constant property function for the specified value. */
-  function constant(x) {
-    return function() { return x; };
+  /** @private Proxy the given property on the layer. */
+  function proxy(name) {
+    return function() {
+        return prop[name](this.parent.index, this.index);
+      };
   }
 
-  /* Override the positional properties with dynamics. */
-  var properties = child.binds.properties;
-  for (var name in positionals) {
-    var p = properties[name];
-    if (!p.original) { // ignore our dynamic binds
-      var d = o[name] = child.propertyValue(name, p.value);
-      d.type = p.type;
-      d.original = p.value;
-    }
-  }
+  /** @private Compute the layout! */
+  function data() {
+    var data = this.layers(),
+        n = data.length,
+        m = data[0].length,
+        orient = this.orient(),
+        horizontal = /^(top|bottom)\b/.test(orient),
+        h = this.parent[horizontal ? "height" : "width"](),
+        binds = this.binds.stack,
+        x = [],
+        y = [],
+        dy = [];
 
-  /* Rebind the dynamic properties. */
-  bind.call(child);
-  child.binds.$stack = o;
-};
-
-/**
- * @private Before the child is built, reassign the positional properties to the
- * appropriate dynamic function, so as to lookup the computed offset.
- */
-pv.Layout.Stack.prototype.prebuild = function(data, child) {
-  var orient = this.orient(),
-      horizontal = /^(top|bottom)$/.test(orient),
-      n = data.length,
-      m = data[0].length,
-      h = this.parent[horizontal ? "height" : "width"](),
-      x = [],
-      y = [],
-      dy = [],
-      z = [],
-      parent = child.parent,
-      properties = child.binds.$stack;
-
-  /* Find the property definitions for dynamic substitution. */
-  var pdy = properties[horizontal ? "height" : "width"],
-      px = horizontal ? properties.left : properties.top,
-      py = properties[orient];
-
-  /* If the x-property is null (the default), use the alternative. */
-  if (px.value == null) px = horizontal ? properties.right : properties.bottom;
-
-  /* Iterate over the data, evaluating the x and dy functions. */
-  var stack = pv.Mark.stack;
-  stack.unshift(null);
-  for (var i = 0; i < n; i++) {
-    dy[i] = [];
-    y[i] = [];
-    z[i] = this.scene[i].visible;
-    parent.index = i;
-    for (var j = 0; j < m; j++) {
-      stack[0] = data[i][j];
-      pv.Mark.prototype.index = child.index = j;
-      if (!i) x[j] = px.original.apply(child, stack);
-      dy[i][j] = z[i] ? pdy.original.apply(child, stack) : 0;
-    }
-  }
-  delete parent.index;
-  delete child.index;
-  stack.shift();
-
-  /* order */
-  var index;
-  switch (this.order()) {
-    case "inside-out": {
-      var max = dy.map(function(v) { return pv.max.index(v); }),
-          map = pv.range(n).sort(function(a, b) { return max[a] - max[b]; }),
-          sums = dy.map(function(v) { return pv.sum(v); }),
-          top = 0,
-          bottom = 0,
-          tops = [],
-          bottoms = [];
-      for (var i = 0; i < n; i++) {
-        var j = map[i];
-        if (top < bottom) {
-          top += sums[j];
-          tops.push(j);
-        } else {
-          bottom += sums[j];
-          bottoms.push(j);
-        }
-      }
-      index = bottoms.reverse().concat(tops);
-      break;
-    }
-    case "reverse": index = pv.range(n - 1, -1, -1); break;
-    default: index = pv.range(n); break;
-  }
-
-  /* offset */
-  switch (this.offset()) {
-    case "silohouette": {
+    /* Iterate over the data, evaluating the x and y functions. */
+    var stack = pv.Mark.stack;
+    stack.unshift(null, null);
+    for (var i = 0; i < n; i++) {
+      dy[i] = [];
+      y[i] = [];
+      this.index = i;
+      stack[1] = data[i];
       for (var j = 0; j < m; j++) {
-        var o = 0;
-        for (var i = 0; i < n; i++) o += dy[i][j];
-        y[index[0]][j] = (h - o) / 2;
+        stack[0] = data[i][j];
+        pv.Mark.prototype.index = this.layer.index = j;
+        if (!i) x[j] = binds.x.apply(this.layer, stack);
+        dy[i][j] = binds.y.apply(this.layer, stack);
       }
-      break;
     }
-    case "wiggle": {
-      var o = 0;
-      for (var i = 0; i < n; i++) o += dy[i][0];
-      y[index[0]][0] = o = (h - o) / 2;
-      for (var j = 1; j < m; j++) {
-        var s1 = 0, s2 = 0, dx = x[j] - x[j - 1];
-        for (var i = 0; i < n; i++) s1 += dy[i][j];
+    delete this.layer.index;
+    delete this.index;
+    stack.splice(0, 2);
+
+    /* order */
+    var index;
+    switch (this.order()) {
+      case "inside-out": {
+        var max = dy.map(function(v) { return pv.max.index(v); }),
+            map = pv.range(n).sort(function(a, b) { return max[a] - max[b]; }),
+            sums = dy.map(function(v) { return pv.sum(v); }),
+            top = 0,
+            bottom = 0,
+            tops = [],
+            bottoms = [];
         for (var i = 0; i < n; i++) {
-          var s3 = (dy[index[i]][j] - dy[index[i]][j - 1]) / (2 * dx);
-          for (var k = 0; k < i; k++) {
-            s3 += (dy[index[k]][j] - dy[index[k]][j - 1]) / dx;
+          var j = map[i];
+          if (top < bottom) {
+            top += sums[j];
+            tops.push(j);
+          } else {
+            bottom += sums[j];
+            bottoms.push(j);
           }
-          s2 += s3 * dy[index[i]][j];
         }
-        y[index[0]][j] = o -= s1 ? s2 / s1 * dx : 0;
+        index = bottoms.reverse().concat(tops);
+        break;
       }
-      break;
+      case "reverse": index = pv.range(n - 1, -1, -1); break;
+      default: index = pv.range(n); break;
     }
-    case "expand": {
-      var nz = pv.sum(z);
-      for (var j = 0; j < m; j++) {
-        y[index[0]][j] = 0;
-        var k = 0;
-        for (var i = 0; i < n; i++) k += dy[i][j];
-        if (k) {
-          k = h / k;
-          for (var i = 0; i < n; i++) dy[i][j] *= k;
-        } else {
-          k = h / nz;
-          for (var i = 0; i < n; i++) dy[i][j] = z[i] ? k : 0;
+
+    /* offset */
+    switch (this.offset()) {
+      case "silohouette": {
+        for (var j = 0; j < m; j++) {
+          var o = 0;
+          for (var i = 0; i < n; i++) o += dy[i][j];
+          y[index[0]][j] = (h - o) / 2;
         }
+        break;
       }
-      break;
+      case "wiggle": {
+        var o = 0;
+        for (var i = 0; i < n; i++) o += dy[i][0];
+        y[index[0]][0] = o = (h - o) / 2;
+        for (var j = 1; j < m; j++) {
+          var s1 = 0, s2 = 0, dx = x[j] - x[j - 1];
+          for (var i = 0; i < n; i++) s1 += dy[i][j];
+          for (var i = 0; i < n; i++) {
+            var s3 = (dy[index[i]][j] - dy[index[i]][j - 1]) / (2 * dx);
+            for (var k = 0; k < i; k++) {
+              s3 += (dy[index[k]][j] - dy[index[k]][j - 1]) / dx;
+            }
+            s2 += s3 * dy[index[i]][j];
+          }
+          y[index[0]][j] = o -= s1 ? s2 / s1 * dx : 0;
+        }
+        break;
+      }
+      case "expand": {
+        for (var j = 0; j < m; j++) {
+          y[index[0]][j] = 0;
+          var k = 0;
+          for (var i = 0; i < n; i++) k += dy[i][j];
+          if (k) {
+            k = h / k;
+            for (var i = 0; i < n; i++) dy[i][j] *= k;
+          } else {
+            k = h / n;
+            for (var i = 0; i < n; i++) dy[i][j] = k;
+          }
+        }
+        break;
+      }
+      default: {
+        for (var j = 0; j < m; j++) y[index[0]][j] = 0;
+        break;
+      }
     }
-    default: {
-      for (var j = 0; j < m; j++) y[index[0]][j] = 0;
-      break;
+
+    /* Propagate the offset to the other series. */
+    for (var j = 0; j < m; j++) {
+      var o = y[index[0]][j];
+      for (var i = 1; i < n; i++) {
+        o += dy[index[i - 1]][j];
+        y[index[i]][j] = o;
+      }
     }
+
+    /* Find the property definitions for dynamic substitution. */
+    var i = orient.indexOf("-"),
+        pdy = horizontal ? "h" : "w",
+        px = i < 0 ? (horizontal ? "l" : "b") : orient.charAt(i + 1),
+        py = i < 0 ? (horizontal ? "b" : "l") : orient.charAt(0);
+    for (var p in prop) prop[p] = none;
+    prop[px] = function(i, j) { return x[j]; };
+    prop[py] = function(i, j) { return y[i][j]; };
+    prop[pdy] = function(i, j) { return dy[i][j]; };
+    return data;
   }
 
-  /* Propagate the offset to the other series. */
-  for (var j = 0; j < m; j++) {
-    var o = y[index[0]][j];
-    for (var i = 1; i < n; i++) {
-      o += dy[index[i - 1]][j];
-      y[index[i]][j] = o;
-    }
-  }
+  this.def("orient", "bottom-left")
+      .def("offset", "zero")
+      .def("order")
+      .def("layers", [[]])
+      .data(data);
 
-  /* Populate the maximum indices for labeling. */
-  this.scene.$stack.max = dy.map(function(array) {
-      return pv.max.index(array);
-    });
-
-  /* Substitute the dynamic properties so the child can build. */
-  px.type = py.type = pdy.type = 3;
-  px.value = function() { return x[this.index]; };
-  py.value = function() { return y[this.parent.index][this.index]; };
-  pdy.value = function() { return dy[this.parent.index][this.index]; };
-  return data;
+  (this.layer = new pv.Mark()
+      .data(pv.identity)
+      .top(proxy("t"))
+      .left(proxy("l"))
+      .right(proxy("r"))
+      .bottom(proxy("b"))
+      .width(proxy("w"))
+      .height(proxy("h")))
+      .parent = this;
 };
+
+pv.Layout.Stack.prototype = pv.extend(pv.Panel);
+
+/** @private Bind the step and size functions, allowing inheritance. */
+pv.Layout.Stack.prototype.bind = function() {
+  pv.Panel.prototype.bind.call(this);
+  var mark = this, binds = this.binds.stack || (this.binds.stack = {});
+  do {
+    if (!binds.x) binds.x = mark.$step;
+    if (!binds.y) binds.y = mark.$size;
+  } while (mark = mark.proto);
+};
+
+/**
+ * The step function; determines the position of the sample within the layer.
+ * This typically corresponds to the independent variable. For example, with the
+ * default "bottom-left" orientation, this function defines the "left" property.
+ *
+ * @param {function} f the step function.
+ * @returns this.
+ */
+pv.Layout.Stack.prototype.step = function(f) {
+  if (arguments.length) {
+    this.$step = typeof f == "function" ? f : function() { return f; };
+    return this;
+  }
+  return this.$step;
+};
+
+/**
+ * The size function; determines the span size of the sample within the layer.
+ * This typically corresponds to the dependent variable. For example, with the
+ * default "bottom-left" orientation, this function defines the "height"
+ * property.
+ *
+ * @param {function} f the size function.
+ * @returns this.
+ */
+pv.Layout.Stack.prototype.size = function(f) {
+  if (arguments.length) {
+    this.$size = typeof f == "function" ? f : function() { return f; };
+    return this;
+  }
+  return this.$size;
+};
+
+/**
+ * The layer data in row-major order.
+ *
+ * @type array[]
+ * @name pv.Layout.Stack.prototype.layers
+ */
+
+/**
+ * The layer orientation. The following values are supported:<ul>
+ *
+ * <li>bottom-left == bottom
+ * <li>bottom-right
+ * <li>top-left == top
+ * <li>top-right
+ * <li>left-top
+ * <li>left-bottom == left
+ * <li>right-top
+ * <li>right-bottom == right
+ *
+ * </ul>. The default value is "bottom-left", which means that the layers will
+ * be built from the bottom-up, and the samples within layers will be laid out
+ * from left-to-right.
+ *
+ * <p>Note that with non-zero baselines, some orientations may give similar
+ * results. For example, offset("silohouette") centers the layers, resulting in
+ * a streamgraph. Thus, the orientations "bottom-left" and "top-left" will
+ * produce similar results, differing only in the layer order.
+ *
+ * @type string
+ * @name pv.Layout.Stack.prototype.orient
+ */
+
+/**
+ * The layer order. The following values are supported:<ul>
+ *
+ * <li><i>null</i>
+ * <li>inside-out
+ * <li>reverse
+ *
+ * </ul>.
+ *
+ * @type string
+ * @name pv.Layout.Stack.prototype.order
+ */
