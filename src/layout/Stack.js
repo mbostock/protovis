f46f26ae3a17ed01f22d3aabe6ad449780381c3c
Mike Bostock <mbostock+gitorious@gmail.com>
2010-04-15 21:14:00 -0400

    Fix (again) stack property definition for top/right orientation.
    The previous commit 33aceb4 was not quite right.

--- src/layout/Stack.js
+++ src/layout/Stack.js
@@ -153,8 +153,8 @@ pv.Layout.Stack = function() {
     /* Find the property definitions for dynamic substitution. */
     var i = orient.indexOf("-"),
         pdy = horizontal ? "h" : "w",
-        px = i < 0 && horizontal ? "l" : orient.charAt(i + 1),
-        py = i < 0 && !horizontal ? "b" : orient.charAt(0);
+        px = i < 0 ? (horizontal ? "l" : "b") : orient.charAt(i + 1),
+        py = orient.charAt(0);
     for (var p in prop) prop[p] = none;
     prop[px] = function(i, j) { return x[j]; };
     prop[py] = function(i, j) { return y[i][j]; };
