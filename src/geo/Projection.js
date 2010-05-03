b88a8c8c61c307d47ea0b845995e3c6c8a2f42a2
Mike Bostock <mbostock+gitorious@gmail.com>
2010-05-03 13:36:55 -0700

    Fix jsdoc for geo package.

--- src/geo/Projection.js
+++ src/geo/Projection.js
@@ -2,11 +2,23 @@
  * Abstract; not implemented. There is no explicit constructor; this class
  * merely serves to document the representation used by {@link pv.Geo.scale}.
  *
- * @class Represents a geographic projection.
+ * @class Represents a geographic projection. This class provides the core
+ * implementation for {@link pv.Geo.scale}s, mapping between geographic
+ * coordinates (latitude and longitude) and normalized screen space in the range
+ * [-1,1]. The remaining mapping between normalized screen space and actual
+ * pixels is performed by <tt>pv.Geo.scale</tt>.
  *
- * TODO describe in more detail how
- * projections map between {@link pv.Geo.LatLng} and {@link pv.Vector} in
- * [-1,1].
+ * <p>Many geographic projections have a point around which the projection is
+ * centered. Rather than have each implementation add support for a
+ * user-specified center point, the <tt>pv.Geo.scale</tt> translates the
+ * geographic coordinates relative to the center point for both the forward and
+ * inverse projection.
+ *
+ * <p>In general, this class should not be used directly, unless the desire is
+ * to implement a new geographic projection. Instead, use <tt>pv.Geo.scale</tt>.
+ * Implementations are not required to implement inverse projections, but are
+ * needed for some forms of interactivity. Also note that some inverse
+ * projections are ambiguous, such as the connecting points in Dymaxian maps.
  *
  * @name pv.Geo.Projection
  * @see pv.Geo.scale
