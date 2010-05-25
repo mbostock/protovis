5adecda2ad0564815dd8f36834b7b1743c01cdd5
Mike Bostock <mbostock+gitorious@gmail.com>
2010-05-24 18:47:40 -0700

    Polish zoom example.

--- www/ex/zoom.js
+++ www/ex/zoom.js
@@ -1,7 +1,7 @@
-var start = new Date(1990, 0, 1).getTime();
+var start = new Date(1990, 0, 1);
 var year = 1000 * 60 * 60 * 24 * 365;
 var data = pv.range(0, 20, .02).map(function(x) {
-    return {x: new Date(start + year * x),
+    return {x: new Date(start.getTime() + year * x),
             y: (1 + .1 * (Math.sin(x * 2 * Math.PI))
                 + Math.random() * .1) * Math.pow(1.18, x)
                 + Math.random() * .1};
